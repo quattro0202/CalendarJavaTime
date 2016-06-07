45f1a90e034a02be68cf2d136f19014b4a6b07cc
Olexander R <quattro02@meta.ua>
2016-06-07 20:58:02 +0300

    update 07.06

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,21 @@
 import calendar.Calendar;
 
-import java.util.Scanner;
-
-
 public class Main {
 
+
+
     public static void main(String[] args) {
-        Integer month = null;
-        Scanner scanner = new Scanner(System.in);
 
-        try{
-            month = scanner.nextInt();
-        }catch (Exception e){
-
-        }
-
-        Calendar calendar;
-
-        if(month != null){
-            calendar = new Calendar(month);
-        }else {
+        Calendar calendar = null;
+        if(args.length == 0){
             calendar = new Calendar();
+        }else{
+            if(args.length == 1){
+                calendar = new Calendar(args[0]);
+            }else {
+                calendar = new Calendar(args[0], args[1]);
+            }
         }
-
         calendar.print();
     }
 }
--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,65 +4,88 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Year;
 import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 
 public class Calendar {
+    private static final int width = 4;
+
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
 
 
 
     private YearMonth yearMonth;
 
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
-
-    public Calendar(int monthNumber){
-        this.setYearMonth(monthNumber);
+    public Calendar(String month){
+        this(month, Year.now().toString());
     }
 
     public Calendar(){
         yearMonth = YearMonth.now();
     }
 
+    public Calendar(String month, String year){
+        String dateStr = month + " " + year;
+
+        DateTimeFormatter fullFormatter = new DateTimeFormatterBuilder()
+                .parseCaseInsensitive().appendPattern("MMMM yyyy").toFormatter(Locale.ENGLISH);
+
+        DateTimeFormatter shortFormatter = new DateTimeFormatterBuilder()
+                .parseCaseInsensitive().appendPattern("MMM yyyy").toFormatter(Locale.ENGLISH);
+
+        try{
+            yearMonth = YearMonth.parse(dateStr, fullFormatter);
+        }catch (Exception e){
+            yearMonth = YearMonth.parse(dateStr, shortFormatter);
+        }
+    }
+
     public void print(){
-        System.out.printf("  Пн ");
-        System.out.printf(" Вт ");
-        System.out.printf(" Ср ");
-        System.out.printf(" Чт ");
-        System.out.printf(" Пт ");
-        System.out.printf(ANSI_YELLOW + " Сб " + ANSI_RESET);
-        System.out.printf(ANSI_YELLOW + " Нд " + ANSI_RESET);
+        System.out.println();
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .parseCaseInsensitive().appendPattern("MMMM yyyy").toFormatter(Locale.ENGLISH);
+        System.out.printf("%" + (width * 7) + "s", yearMonth.format(formatter));
+        System.out.println();
+        for (int i = 0; i < DayOfWeek.values().length; i++) {
+            String day = DayOfWeek.values()[i].getDisplayName(TextStyle.SHORT, Locale.UK);
+            if(i < DayOfWeek.values().length - 2){
+                System.out.printf("%" + width + "s", day);
+            }else {
+                System.out.printf("%" + (width + 9) + "s", ANSI_YELLOW + day + ANSI_RESET);
+            }
+
+        }
         System.out.println();
 
-        DayOfWeek currentDayOfWeek = DayOfWeek.MONDAY;
+        DayOfWeek currentDayOfWeek = DayOfWeek.of(1);
         LocalDate day = yearMonth.atDay(1);
 
 
         while(day.isBefore(yearMonth.atEndOfMonth().plusDays(1))){
             if(currentDayOfWeek.equals(day.getDayOfWeek())){
                 if(day.equals(LocalDate.now())){
-                    System.out.printf("%13s", ANSI_RED + day.getDayOfMonth() + ANSI_RESET);
+                    System.out.printf("%" + (width +  9) + "s", ANSI_RED + day.getDayOfMonth() + ANSI_RESET);
                 }else {
                     if(currentDayOfWeek.equals(DayOfWeek.SUNDAY) || currentDayOfWeek.equals(DayOfWeek.SATURDAY)){
-                        System.out.printf("%13s", ANSI_YELLOW + day.getDayOfMonth() + ANSI_RESET);
+                        System.out.printf("%" + (width +  9) + "s", ANSI_YELLOW + day.getDayOfMonth() + ANSI_RESET);
                     }else {
-                        System.out.printf("%4s", day.getDayOfMonth());
+                        System.out.printf("%" + width + "s", day.getDayOfMonth());
                     }
                 }
 
 
-                if(currentDayOfWeek.equals(DayOfWeek.SUNDAY))
+                if(currentDayOfWeek.getValue() == 7)
                     System.out.println();
             }else{
-                System.out.print("    ");
+                for (int i = 0; i < width; i++) {
+                    System.out.print(" ");
+                }
                 currentDayOfWeek = currentDayOfWeek.plus(1);
                 continue;
             }
@@ -70,19 +93,5 @@ public class Calendar {
             day = day.plusDays(1);
         }
         System.out.println();
-    }
-
-    public int getYearMonth() {
-        return yearMonth.getMonthValue();
-    }
-
-    public void setYearMonth(int monthNumber) {
-
-        if(monthNumber > 0 && monthNumber < 13){
-            yearMonth = Year.now().atMonth(monthNumber);
-        }else {
-            throw new IllegalArgumentException("Bad variable: monthNumber = " + monthNumber + ". It may be in interval 1..12.");
-        }
-
     }
 }
